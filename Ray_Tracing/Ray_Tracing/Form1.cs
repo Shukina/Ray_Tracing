79b85c014dec20b83889fdb4ffa178f094918176
Аня <anya15-98@mail.ru>
2017-05-23 13:32:45 +0300

    first

---
+++ Ray_Tracing/Ray_Tracing/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using OpenTK;
+using OpenTK.Graphics.OpenGL;
+
+namespace Ray_Tracing
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            glControl1.Invalidate();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void glControl1_Paint(object sender, PaintEventArgs e)
+        {
+            Shader m = new Shader();
+
+            Console.WriteLine(m.glslVersion);
+            Console.WriteLine(m.glVersion);
+
+            m.InitShaders(glControl1.Width / (float)glControl1.Height);
+
+            GL.Viewport(0, 0, glControl1.Width, glControl1.Height);
+
+            GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+            GL.Enable(EnableCap.DepthTest);
+
+
+            GL.EnableVertexAttribArray(m.attribute_vpos);
+
+
+            Console.WriteLine("OK");
+            GL.DrawArrays(PrimitiveType.Quads, 0, 4);
+
+
+            GL.DisableVertexAttribArray(m.attribute_vpos);
+
+            glControl1.SwapBuffers();
+            GL.UseProgram(0);
+        }
+    }
+}
---
+++ Ray_Tracing/Ray_Tracing/Shader.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenTK;
+using OpenTK.Graphics.OpenGL;
+using System.Drawing;
+using System.IO;
+using OpenTK.Input;
+
+namespace Ray_Tracing
+{
+    class Shader
+    {
+
+        public string glVersion;
+        public string glslVersion;
+        public int attribute_vpos;
+        int BasicProgramID; //Номер дескриптора на графической карте
+        int BasicVertexShader; //Адрес вершинного шейдера  
+        int BasicFragmentShader; //Адрес фрагментного шейдера
+        //int attribute_vpos; //Адрес параметра позиции
+        int vboVertexPosition; //Адрес буфера вершин объекта для нашего параметра позиции
+        int uniform_pos;
+        int uniform_aspect;
+        float aspect;
+        Vector3 camera_position;
+        Vector3[] vertdata; //Массив позиций вершин
+
+
+        void loadShader(String filename, ShaderType type, int program, out int address)
+        {
+            address = GL.CreateShader(type);
+            using (System.IO.StreamReader sr = new StreamReader(filename))
+            {
+                GL.ShaderSource(address, sr.ReadToEnd());
+            }
+            GL.CompileShader(address);
+            GL.AttachShader(program, address);
+            Console.WriteLine(GL.GetShaderInfoLog(address));
+        }
+
+        public void InitShaders(float character_size)
+        {
+            BasicProgramID = GL.CreateProgram(); // создание объекта программы
+            loadShader("..\\..\\..\\raytracing.vert", ShaderType.VertexShader, BasicProgramID, out BasicVertexShader);
+            loadShader("..\\..\\..\\raytracing3.frag", ShaderType.FragmentShader, BasicProgramID, out BasicFragmentShader);
+            // компановка программы
+            GL.LinkProgram(BasicProgramID);
+
+            // Проверяем успех компоновки
+
+            int status = 0;
+            GL.GetProgram(BasicProgramID, GetProgramParameterName.LinkStatus, out status);
+
+            //Console.WriteLine("InfoLog:");
+
+            Console.WriteLine(GL.GetProgramInfoLog(BasicProgramID));
+            vertdata = new Vector3[] {new Vector3(-1f, -1f, 0f),
+                                      new Vector3( 1f, -1f, 0f),
+                                      new Vector3( 1f, 1f, 0f),
+                                      new Vector3(-1f, 1f, 0f) };
+
+            //рисуем квад
+            //создали один буфер, связали с атрибутом и заполнили данными
+            //vbo_position
+            GL.GenBuffers(1, out vboVertexPosition);
+            GL.BindBuffer(BufferTarget.ArrayBuffer, vboVertexPosition);
+            GL.BufferData<Vector3>(BufferTarget.ArrayBuffer, (IntPtr)(vertdata.Length * Vector3.SizeInBytes), vertdata, BufferUsageHint.StaticDraw);
+            GL.VertexAttribPointer(attribute_vpos, 3, VertexAttribPointerType.Float, false, 0, 0);
+            //vbo
+            GL.Uniform3(uniform_pos, camera_position); //этого нет
+            GL.Uniform1(uniform_aspect, aspect);       //этого тоже
+            GL.UseProgram(BasicProgramID);
+            GL.BindBuffer(BufferTarget.ArrayBuffer, 0);
+        }
+        }
+    }
